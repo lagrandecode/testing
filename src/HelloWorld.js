da83f54807c6e437d333bebc0a015600efa615d7
lagrandecode <seunogunmolufirst12@gmail.com>
2024-09-29 14:30:50 -0400

    "commit"

---
+++ src/HelloWorld.js
@@ -0,0 +1,8 @@
+import React, {Component } from "react";
+class HelloWorld extends Component {
+    render(){
+        return (
+            <div className="helloContainer">
+                <h1>Hello World show
+                </h1>
+            </div>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
--- src/index.js
+++ src/index.js
@@ -3,13 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HelloWorld from "./HelloWorld";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
